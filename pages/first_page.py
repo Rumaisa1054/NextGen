215135db5197ff67927756fdb0280f2d2a37ade5
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2024-01-21 23:48:48 +0500

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -109,79 +109,77 @@ def main():
             ss = "Answer these questions : " + Q
             ans = st.text_area(ss)
             #ans = st.text_input("Answer these questions : ")
-            if st.button("Submit-Answer"):
-                st.title("Patient History : ")
-                st.text(history + "\n" + ans)
-                st.title("Food Nutritional Info : ")
-                st.text(json_string)
-                prompt = f"You are an experienced doctor. Given a patient with History : {history + ans} , Make a strict Suggestion on whether He/she can eat a food Item with nutritional information given : {json_string} or he should striclty avoid eating it"
+            st.title("Patient History : ")
+            st.text(history + "\n" + ans)
+            st.title("Food Nutritional Info : ")
+            st.text(json_string)
+            promptt = f"You are an experienced doctor. Given a patient with History : {history + ans} , Make a strict Suggestion on whether He/she can eat a food Item with nutritional information given : {json_string} or he should striclty avoid eating it"
+            ######################################################################################################
+            # In this section, we set the user authentication, user and app ID, model details, and the URL of
+            # the text we want as an input. Change these strings to run your own example.
+            ######################################################################################################
 
-                ######################################################################################################
-                # In this section, we set the user authentication, user and app ID, model details, and the URL of
-                # the text we want as an input. Change these strings to run your own example.
-                ######################################################################################################
+            # Your PAT (Personal Access Token) can be found in the portal under Authentification
+            PAT = clarifai_pat
+            # Specify the correct user_id/app_id pairings
+            # Since you're making inferences outside your app's scope
+            USER_ID = 'openai'
+            APP_ID = 'chat-completion'
+            # Change these to whatever model and text URL you want to use
+            MODEL_ID = 'GPT-3_5-turbo'
+            MODEL_VERSION_ID = '4471f26b3da942dab367fe85bc0f7d21'
+            RAW_TEXT = promptt
+            # To use a hosted text file, assign the url variable
+            # TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
+            # Or, to use a local text file, assign the url variable
+            # TEXT_FILE_LOCATION = 'YOUR_TEXT_FILE_LOCATION_HERE'
 
-                # Your PAT (Personal Access Token) can be found in the portal under Authentification
-                PAT = clarifai_pat
-                # Specify the correct user_id/app_id pairings
-                # Since you're making inferences outside your app's scope
-                USER_ID = 'openai'
-                APP_ID = 'chat-completion'
-                # Change these to whatever model and text URL you want to use
-                MODEL_ID = 'GPT-3_5-turbo'
-                MODEL_VERSION_ID = '4471f26b3da942dab367fe85bc0f7d21'
-                RAW_TEXT = prompt
-                # To use a hosted text file, assign the url variable
-                # TEXT_FILE_URL = 'https://samples.clarifai.com/negative_sentence_12.txt'
-                # Or, to use a local text file, assign the url variable
-                # TEXT_FILE_LOCATION = 'YOUR_TEXT_FILE_LOCATION_HERE'
-
-                ############################################################################
-                # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-                ############################################################################
+            ############################################################################
+            # YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+            ############################################################################
 
 
 
-                channel = ClarifaiChannel.get_grpc_channel()
-                stub = service_pb2_grpc.V2Stub(channel)
+            channel = ClarifaiChannel.get_grpc_channel()
+            stub = service_pb2_grpc.V2Stub(channel)
 
-                metadata = (('authorization', 'Key ' + PAT),)
+            metadata = (('authorization', 'Key ' + PAT),)
 
-                userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
+            userDataObject = resources_pb2.UserAppIDSet(user_id=USER_ID, app_id=APP_ID)
 
-                # To use a local text file, uncomment the following lines
-                # with open(TEXT_FILE_LOCATION, "rb") as f:
+            # To use a local text file, uncomment the following lines
+            # with open(TEXT_FILE_LOCATION, "rb") as f:
                 #    file_bytes = f.read()
 
-                post_model_outputs_response = stub.PostModelOutputs(
-                    service_pb2.PostModelOutputsRequest(
-                        user_app_id=userDataObject,
-                        # The userDataObject is created in the overview and is required when using a PAT
-                        model_id=MODEL_ID,
-                        version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
-                        inputs=[
-                            resources_pb2.Input(
-                                data=resources_pb2.Data(
-                                    text=resources_pb2.Text(
-                                        raw=RAW_TEXT
-                                        # url=TEXT_FILE_URL
-                                        # raw=file_bytes
-                                    )
+            post_model_outputs_response = stub.PostModelOutputs(
+                service_pb2.PostModelOutputsRequest(
+                    user_app_id=userDataObject,
+                    # The userDataObject is created in the overview and is required when using a PAT
+                    model_id=MODEL_ID,
+                    version_id=MODEL_VERSION_ID,  # This is optional. Defaults to the latest model version
+                    inputs=[
+                        resources_pb2.Input(
+                            data=resources_pb2.Data(
+                                text=resources_pb2.Text(
+                                    raw=RAW_TEXT
+                                    # url=TEXT_FILE_URL
+                                    # raw=file_bytes
                                 )
                             )
-                        ]
-                    ),
-                    metadata=metadata
-                )
-                if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
-                    print(post_model_outputs_response.status)
-                    raise Exception(f"Post model outputs failed, status: {post_model_outputs_response.status.description}")
+                        )
+                    ]
+                ),
+                metadata=metadata
+            )
+            if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+                print(post_model_outputs_response.status)
+                raise Exception(f"Post model outputs failed, status: {post_model_outputs_response.status.description}")
 
-                # Since we have one input, one output will exist here
-                output1 = post_model_outputs_response.outputs[0]
-                
-                st.title(" RECoMMENDATION : ")
-                st.text(output1.data.text.raw)
+            # Since we have one input, one output will exist here
+            output1 = post_model_outputs_response.outputs[0]
+            
+            st.title(" RECoMMENDATION : ")
+            st.text(output1.data.text.raw)
 
 if __name__ == '__main__':
     main()
