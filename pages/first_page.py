11f4dbb7f325b65a875a9115918d1f8ca2402ead
Rumaisa <105513477+Rumaisa1054@users.noreply.github.com>
2024-01-21 22:56:02 +0500

    Update first_page.py

--- pages/first_page.py
+++ pages/first_page.py
@@ -109,7 +109,7 @@ def main():
             ss = "Answer these questions : " + Q
             ans = st.text_input(ss, value="Since my Type 2 diabetes diagnosis in 2020, I've made some serious changes to my diet. I cut down on the sugary stuff and started loading up on veggies, lean proteins, and whole grains. The sweet treats are a rare sight now, and I've become best friends with my plate of greens. Ever since tweaking my diet, my energy levels have perked up, and the unquenchable thirst has eased up a lot. It's like my body's found a new rhythm, and I'm not constantly feeling like I'm running on empty. The change has been refreshing, and I'm learning to navigate this healthier path.")
             #ans = st.text_input("Answer these questions : ")
-            if st.button("Submit"):
+            if st.button("Submit-Answer"):
                 prompt = f"You are an experienced doctor. Given a patient with History : {history + ans} , Make a strict Suggestion on whether He/she can eat a food Item with nutritional information given : {json_string} or he should striclty avoid eating it"
 
                 ######################################################################################################
